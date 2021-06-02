3bee6226e0896ab543a3a2f45c5334544356b445
carlaburguete <77242087+carlaburguete@users.noreply.github.com>
2021-06-02 20:37:47 +0200

    Modificado auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,4 @@
  @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -15,45 +14,26 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
-                            <label for="nom_usuario" class="col-md-4 col-form-label text-md-right">{{'Usuario'}}</label>
+                            <label for="nom_usuario" class="col-md-4 col-form-label text-md-right">Usuario</label>
 
                             <div class="col-md-6">
-                                <input id="text" type="text" class="form-control-file" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="text" type="text" class="form-control-file" name="nom_usuario" required>
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control-file @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="password" type="password" class="form-control-file " name="password" required>
                             </div>
                         </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-info">
-                                    {{ __('ENVIAR') }}
+                                    ENVIAR
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-outline-info" href="{{ route('password.request') }}">
-                                        {{ __('¿Has olvidado tu contraseña?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
